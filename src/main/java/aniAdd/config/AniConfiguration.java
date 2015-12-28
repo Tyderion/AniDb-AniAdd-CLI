e3dce6ece20e71034619c6fbf4a4fdb14b4e624f
Gabriel Nadler <nadler.gabriel@gmail.com>
2015-12-28 16:40:33 +0100

    add AniConfiguration Mappings for Mod_Memory

--- src/main/java/aniAdd/config/AniConfiguration.java
+++ src/main/java/aniAdd/config/AniConfiguration.java
@@ -1,6 +1,8 @@
 package aniAdd.config;
 
 
+import java.util.Map;
+
 /**
  * Created by Archie on 23.12.2015.
  */
@@ -9,154 +11,197 @@ public class AniConfiguration {
         UNKOWN, INTERNAL, EXTERNAL, DELETE
     }
 
-    protected boolean mEnableFileMove;
+    protected Value<Boolean> mEnableFileMove;
 
-    protected boolean mEnableFileRenaming;
-    protected String mMoveToFolder;
-    protected boolean mOverwriteMLEntries;
-    protected boolean mRecursivelyDeleteEmptyFolders;
-    protected boolean mRenameFiles;
-    protected boolean mRenameRelatedFiles;
-    protected boolean mRenameTypeAniDBFileName;
-    protected StorageType mSetStorageType;
+    protected Value<Boolean> mEnableFileRenaming;
+    protected Value<String> mMoveToFolder;
+    protected Value<Boolean> mOverwriteMLEntries;
+    protected Value<Boolean> mRecursivelyDeleteEmptyFolders;
+    protected Value<Boolean> mRenameFiles;
+    protected Value<Boolean> mRenameRelatedFiles;
+    protected Value<Boolean> mRenameTypeAniDBFileName;
+    protected Value<StorageType> mSetStorageType;
 
-    protected String mFolderToLoad;
-    protected boolean mSetWatched;
+    protected Value<String> mFolderToLoad;
+    protected Value<Boolean> mSetWatched;
 
-    protected boolean mAddToMylist;
-    protected boolean mAdvancedMode;
-    protected String mTestString;
-    protected boolean mMoveTypeUseFolder;
-    protected String mTagSystemCode;
+    protected Value<Boolean> mAddToMylist;
+    protected Value<Boolean> mAdvancedMode;
+    protected Value<Boolean> mMoveTypeUseFolder;
+    protected Value<String> mTagSystemCode;
+
+    protected String mDirectory;
+
+    public String getDirectory() {
+        return mDirectory;
+    }
+
+    public void setDirectory(String directory) {
+        mDirectory = directory;
+    }
 
     public boolean isAddToMylist() {
-        return mAddToMylist;
+        return mAddToMylist.value;
     }
 
     public void setAddToMylist(boolean addToMylist) {
-        mAddToMylist = addToMylist;
+        mAddToMylist = new Value<Boolean>(ToMod_MemoryMapper.AddToMylist, addToMylist);
     }
 
     public boolean isAdvancedMode() {
-        return mAdvancedMode;
+        return mAdvancedMode.value;
     }
 
     public void setAdvancedMode(boolean advancedMode) {
-        mAdvancedMode = advancedMode;
-    }
-
-    public String getTestString() {
-        return mTestString;
-    }
-
-    public void setTestString(String testString) {
-        mTestString = testString;
+        mAdvancedMode = new Value<Boolean>(ToMod_MemoryMapper.AdvancedMode, advancedMode);
     }
 
     public String getTagSystemCode() {
-        return mTagSystemCode;
+        return mTagSystemCode.value;
     }
 
     public void setTagSystemCode(String tagSystemCode) {
-        mTagSystemCode = tagSystemCode;
+        mTagSystemCode = new Value<String>(ToMod_MemoryMapper.TagSystemCode, tagSystemCode);
     }
 
     public boolean isMoveTypeUseFolder() {
-        return mMoveTypeUseFolder;
+        return mMoveTypeUseFolder.value;
     }
 
 
     public boolean isEnableFileRenaming() {
-        return mEnableFileRenaming;
+        return mEnableFileRenaming.value;
     }
 
     public void setEnableFileRenaming(boolean enableFileRenaming) {
-        mEnableFileRenaming = enableFileRenaming;
+        mEnableFileRenaming = new Value<Boolean>(ToMod_MemoryMapper.EnableFileRenaming, enableFileRenaming);
     }
 
     public String getMoveToFolder() {
-        return mMoveToFolder;
+        return mMoveToFolder.value;
     }
 
     public void setMoveToFolder(String moveToFolder) {
-        mMoveToFolder = moveToFolder;
+        mMoveToFolder = new Value<String>(ToMod_MemoryMapper.MoveToFolder, moveToFolder);
     }
 
     public boolean isOverwriteMLEntries() {
-        return mOverwriteMLEntries;
+        return mOverwriteMLEntries.value;
     }
 
     public void setOverwriteMLEntries(boolean overwriteMLEntries) {
-        mOverwriteMLEntries = overwriteMLEntries;
+        mOverwriteMLEntries = new Value<Boolean>(ToMod_MemoryMapper.OverwriteMLEntries, overwriteMLEntries);
     }
 
     public boolean isRecursivelyDeleteEmptyFolders() {
-        return mRecursivelyDeleteEmptyFolders;
+        return mRecursivelyDeleteEmptyFolders.value;
     }
 
     public void setRecursivelyDeleteEmptyFolders(boolean recursivelyDeleteEmptyFolders) {
-        mRecursivelyDeleteEmptyFolders = recursivelyDeleteEmptyFolders;
+        mRecursivelyDeleteEmptyFolders = new Value<Boolean>(ToMod_MemoryMapper.RecursivelyDeleteEmptyFolders, recursivelyDeleteEmptyFolders);
     }
 
     public boolean isRenameFiles() {
-        return mRenameFiles;
+        return mRenameFiles.value;
     }
 
     public void setRenameFiles(boolean renameFiles) {
-        mRenameFiles = renameFiles;
+        mRenameFiles = new Value<Boolean>(ToMod_MemoryMapper.RenameFiles, renameFiles);
     }
 
     public boolean isRenameRelatedFiles() {
-        return mRenameRelatedFiles;
+        return mRenameRelatedFiles.value;
     }
 
     public void setRenameRelatedFiles(boolean renameRelatedFiles) {
-        mRenameRelatedFiles = renameRelatedFiles;
+        mRenameRelatedFiles = new Value<Boolean>(ToMod_MemoryMapper.RenameRelatedFiles, renameRelatedFiles);
     }
 
     public boolean isRenameTypeAniDBFileName() {
-        return mRenameTypeAniDBFileName;
+        return mRenameTypeAniDBFileName.value;
     }
 
     public void setRenameTypeAniDBFileName(boolean renameTypeAniDBFileName) {
-        mRenameTypeAniDBFileName = renameTypeAniDBFileName;
+        mRenameTypeAniDBFileName = new Value<Boolean>(ToMod_MemoryMapper.RenameTypeAniDBFileName, renameTypeAniDBFileName);
     }
 
     public StorageType getSetStorageType() {
-        return mSetStorageType;
+        return mSetStorageType.value;
     }
 
     public void setSetStorageType(StorageType setStorageType) {
-        mSetStorageType = setStorageType;
+        mSetStorageType = new Value<StorageType>(ToMod_MemoryMapper.SetStorageType, setStorageType);
     }
 
     public void setMoveTypeUseFolder(boolean moveTypeUseFolder) {
-        mMoveTypeUseFolder = moveTypeUseFolder;
+        mMoveTypeUseFolder = new Value<Boolean>(ToMod_MemoryMapper.MoveTypeUseFolder, moveTypeUseFolder);
     }
 
     public boolean isEnableFileMove() {
-        return mEnableFileMove;
+        return mEnableFileMove.value;
     }
 
     public void setEnableFileMove(boolean enableFileMove) {
-        mEnableFileMove = enableFileMove;
+        mEnableFileMove = new Value<Boolean>(ToMod_MemoryMapper.EnableFileMove, enableFileMove);
     }
 
     public String getFolderToLoad() {
-        return mFolderToLoad;
+        return mFolderToLoad.value;
     }
 
     public void setFolderToLoad(String folderToLoad) {
-        mFolderToLoad = folderToLoad;
+        mFolderToLoad = new Value<String>(ToMod_MemoryMapper.FolderToLoad, folderToLoad);
     }
 
     public boolean isSetWatched() {
-        return mSetWatched;
+        return mSetWatched.value;
     }
 
     public void setSetWatched(boolean setWatched) {
-        mSetWatched = setWatched;
+        mSetWatched = new Value<Boolean>(ToMod_MemoryMapper.SetWatched, setWatched);
     }
 
+    private enum ToMod_MemoryMapper {
+        EnableFileMove("FileMove"),
+        EnableFileRenaming("GUI_EnableFileRenaming"),
+        MoveToFolder("GUI_MoveToFolder"),
+        OverwriteMLEntries("GUI_OverwriteMLEntries"),
+        RecursivelyDeleteEmptyFolders("GUI_RecursivelyDeleteEmptyFolders"),
+        RenameFiles("GUI_RenameFiles"),
+        RenameRelatedFiles("GUI_RenameRelatedFiles"),
+        RenameTypeAniDBFileName("GUI_RenameTypeAniDBFileName"),
+        SetStorageType("GUI_SetStorageType"),
+        FolderToLoad("GUI_FolderToAddOnLoad"),
+        SetWatched("GUI_SetWatched"),
+        AddToMylist("GUI_AddToMyList"),
+        AdvancedMode("GUI_AdvMode"),
+        MoveTypeUseFolder("GUI_MoveTypeUseFolder"),
+        TagSystemCode("GUI_TagSystemCode");
+        private final String representation;
 
+        private ToMod_MemoryMapper(String s) {
+            representation = s;
+        }
+
+        @Override
+        public String toString() {
+            return this.representation;
+        }
+
+    }
+
+    private class Value<T> {
+        public final ToMod_MemoryMapper mapping;
+        public final T value;
+
+        private Value(ToMod_MemoryMapper mapping, T value) {
+            this.mapping = mapping;
+            this.value = value;
+        }
+    }
+
+    public Map<String, String> ToMod_Memory() {
+        // TODO: Implement
+        return null;
+    }
 }
