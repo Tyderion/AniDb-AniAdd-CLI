bdb9ac5f84b51049baaa0ad6c90a6790b627bd0f
Gabriel Nadler <nadler.gabriel@gmail.com>
2024-07-14 23:40:20 +0200

    logging.properties file

---
+++ src/main/java/aniAdd/startup/LoggerConfig.java
@@ -0,0 +1,2 @@
+package aniAdd.startup;public class LoggerConfig {
+}
--- src/main/java/aniAdd/startup/Main.java
+++ src/main/java/aniAdd/startup/Main.java
@@ -1,12 +1,29 @@
 package aniAdd.startup;
 
 import aniAdd.startup.commands.CliCommand;
+import lombok.val;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.LogManager;
+import java.util.stream.Collectors;
 
 public class Main {
 
+    private final static String LOGGING_FORMAT_KEY = "java.util.logging.SimpleFormatter.format";
+
+    static {
+        LoggerConfig.configureLogger();
+    }
+
+    private static final String LOG_LEVEL = "FINEST";
+
     public static void main(String[] args) {
-        System.setProperty("java.util.logging.SimpleFormatter.format",
-                "%1$tF %1$tT %4$s %2$s %5$s%6$s%n");
+
         new picocli.CommandLine(new CliCommand()).execute(args);
     }
 }
