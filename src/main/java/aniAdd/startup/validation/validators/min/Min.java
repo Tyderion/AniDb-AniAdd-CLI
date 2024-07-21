ef81daf06bad5ae62eecf504cbf8877c86b1a29e
Gabriel Nadler <nadler.gabriel@gmail.com>
2024-07-21 23:39:04 +0200

    Add some javadoc for validations

--- src/main/java/aniAdd/startup/validation/validators/min/Min.java
+++ src/main/java/aniAdd/startup/validation/validators/min/Min.java
@@ -4,9 +4,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Validates that the given field is greater than or equal to the specified value
+
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({java.lang.annotation.ElementType.FIELD})
 public @interface Min {
-    public long value();
-    public String message() default "{field} cannot be smaller than {value}";
+    /**
+     * Minimum value
+     */
+    long value();
+
+    /**
+     * Message to be displayed when validation fails
+     * {field} will be replaced with the field name
+     * {value} will be replaced with the value
+     */
+    String message() default "{field} cannot be smaller than {value}";
 }
--- src/main/java/aniAdd/startup/validation/validators/nonempty/NonEmpty.java
+++ src/main/java/aniAdd/startup/validation/validators/nonempty/NonEmpty.java
@@ -7,6 +7,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Validates that the given field is not an empty string
+
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD})
 public @interface NonEmpty {
