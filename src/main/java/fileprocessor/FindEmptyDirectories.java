0bf3d4ac5857cf77221c4f894cca50108784b3ff
Gabriel Nadler <nadler.gabriel@gmail.com>
2024-07-21 20:52:09 +0200

    workgin find empty dirs

--- src/main/java/fileprocessor/FindEmptyDirectories.java
+++ src/main/java/fileprocessor/FindEmptyDirectories.java
@@ -1,6 +1,7 @@
 package fileprocessor;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.java.Log;
 import lombok.val;
 
@@ -10,11 +11,14 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @Log
 @RequiredArgsConstructor
@@ -24,23 +28,44 @@ public class FindEmptyDirectories implements Callable<List<Path>> {
 
     @Override
     public List<Path> call() throws Exception {
-        val files = recursivelyGetEmptyDirs(Paths.get(directory)).collect(Collectors.toList());
-        files.forEach(f -> log.finest(STR."Found empty directory: \{f.toAbsolutePath().toString()}"));
+        val result = getEmptyDirectories(Paths.get(directory));
+//
+//        Iterable<Path> paths = () -> recursivelyGetEmptyDirs(Paths.get(directory)).collect(Collectors.toCollection(ArrayDeque::new))
+//                .descendingIterator();
+//        val files = StreamSupport.stream(paths.spliterator(), false).toList();
+        val files = result.getEmptyChildren();
+        files.forEach(f -> log.info(STR."Found empty directory: \{f.toAbsolutePath().toString()}"));
         log.info(STR."Number of found files: \{files.size()}");
         return files;
     }
 
-    private Stream<Path> recursivelyGetEmptyDirs(Path folder) {
-        if (!Files.exists(folder) || !Files.isDirectory(folder)) {
-            return Stream.empty();
-        }
-        try (val emptySubfolders = Files.find(folder, 1, (path, attr) -> attr.isDirectory() && isDirEmpty(path))) {
-            try (val otherSubfolders = Files.find(folder, 1, (path, attr) -> attr.isDirectory())) {
-                return otherSubfolders.reduce(emptySubfolders, (acc, ele) -> Stream.concat(acc, recursivelyGetEmptyDirectoriesToScrape(ele)), Stream::concat);
+    private FindResult getEmptyDirectories(Path root) {
+        try (val files = Files.find(root, 1, (path, attr) -> !path.toString().equals(root.toString()))) {
+            val childFiles = files.toList();
+            if (childFiles.isEmpty()) {
+                return new FindResult(0, List.of(root));
             }
+            val emptyChildDirectories = childFiles.stream().filter(Files::isDirectory).map(this::getEmptyDirectories).toList();
+            val fileCount = childFiles.stream().filter(Files::isRegularFile).count();
+            val subResult = emptyChildDirectories.stream().reduce(new FindResult((int) fileCount, List.of()),
+                    (a, b) -> new FindResult(a.childrenCount + b.childrenCount + 1,
+                            Stream.concat(a.emptyChildren.stream(), b.emptyChildren.stream()).toList()));
+            if (subResult.childrenCount == subResult.getEmptyChildren().size()) {
+                return new FindResult(subResult.childrenCount + (int) fileCount,
+                        Stream.concat(subResult.getEmptyChildren().stream(), Stream.of(root)).toList());
+            } else {
+                return subResult;
+            }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Value
+    private class FindResult {
+        int childrenCount;
+        List<Path> emptyChildren;
     }
 
     private static boolean isDirEmpty(final Path directory) {
