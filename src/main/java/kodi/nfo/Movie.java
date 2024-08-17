2a7c51dc0aa455023249770d7b62439561b1970f
Gabriel Nadler <nadler.gabriel@gmail.com>
2024-08-17 11:24:29 +0200

    Add uncomitted files

---
+++ src/main/java/kodi/nfo/Movie.java
@@ -0,0 +1,51 @@
+package kodi.nfo;
+
+import kodi.common.UniqueId;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@SuperBuilder
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class Movie extends RootTag {
+    String title;
+    String originalTitle;
+
+    @Singular List<Rating> ratings;
+
+    String outline;
+    String plot;
+    String tagline;
+
+    int runtimeInSeconds;
+
+    String thumbnail;
+    @Singular List<Artwork> fanarts;
+    @Singular List<UniqueId> uniqueIds;
+    @Singular List<String> tags;
+    @Singular List<String> genres;
+    @Singular List<String> credits;
+    @Singular List<String> directors;
+    String trailer;
+    String studio;
+
+    LocalDate lastPlayed;
+    public boolean isWatched() {
+        return lastPlayed != null;
+    }
+    Set set;
+    LocalDate premiered;
+    StreamDetails streamDetails;
+    @Singular List<Actor> actors;
+
+    @Builder
+    @Value
+    public static class Set {
+        String name;
+        String overview;
+    }
+}
